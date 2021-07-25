618cceeb1a0249ad9673eb372e4b2ad342ddd098
Wolfleader <james.morgan@iinet.net.au>
2021-07-25 14:36:48 +0800

    ernames

--- Assets/Scripts/CarControllerFollower.cs
+++ Assets/Scripts/CarControllerFollower.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SphereOffsetFollower : MonoBehaviour
+public class CarControllerFollower : MonoBehaviour
 {
     [SerializeField] private GameObject car;
 
