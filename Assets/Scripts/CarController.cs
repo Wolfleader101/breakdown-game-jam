fa8b681aad2b62029728921eff17b44b9427d633
Wolfleader <james.morgan@iinet.net.au>
2021-07-25 16:02:22 +0800

    car cpontroller fix

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -9,41 +9,68 @@ public class CarController : MonoBehaviour
     [SerializeField] private float forwardAccel = 20f;
     [SerializeField] private float reverseAccel = 10f;
     [SerializeField] private float turnStrength = 50f;
-    [SerializeField] private float maxSpeed =  1000f;
+    [SerializeField] private float maxSpeed = 1000f;
     [SerializeField] private Rigidbody rb;
+    [SerializeField] private LayerMask groundMask;
+    [SerializeField] private Transform rayPos;
+    [SerializeField] private float rayLength = 1f;
+    [SerializeField] private float airDrag = 0.1f;
+    [SerializeField] private float downwardsForce = 15f;
 
     private float _accelDir = 0f;
     private float _turnDir = 0f;
+
+    private float _defaultDrag;
+    private bool _isGrounded = false;
 
 
     // Start is called before the first frame update
     void Start()
     {
         rb.transform.parent = null;
+        _defaultDrag = rb.drag;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f,_turnDir * turnStrength * Time.deltaTime, 0f));
+        if (_isGrounded)
+        {
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles +
+                                                  new Vector3(0f, _turnDir * turnStrength * Time.deltaTime * _accelDir,
+                                                      0f));
+        }
+
         transform.position = rb.transform.position;
-        
-      
     }
 
     private void FixedUpdate()
     {
-        Move(_accelDir, forwardAccel);
-        // if (rb.velocity.magnitude < maxSpeed)
-        // {
-        //     Move(_moveDir, forwardAccel);
-        // }
-        // else
-        // {
-        //     var velocity = rb.velocity;
-        //     velocity = new Vector3(velocity.x, velocity.y, maxSpeed);
-        //     rb.velocity = velocity;
-        // }
+        _isGrounded = false;
+        RaycastHit hit;
+        if (Physics.Raycast(rayPos.position, -transform.up, out hit, rayLength, groundMask))
+        {
+            Debug.DrawRay(rayPos.position, -transform.up * rayLength, Color.yellow);
+            _isGrounded = true;
+        }
+
+        if (_isGrounded)
+        {
+            rb.drag = _defaultDrag;
+            if (_accelDir > 0)
+            {
+                Move(_accelDir, forwardAccel);
+            }
+            else if (_accelDir < 0)
+            {
+                Move(_accelDir, reverseAccel);
+            }
+        }
+        else
+        {
+            rb.drag = airDrag;
+            rb.AddForce(Vector3.down * (downwardsForce * 100));
+        }
     }
 
     private void Move(float accelDir, float speed)
@@ -54,14 +81,15 @@ public class CarController : MonoBehaviour
 
     public void OnAccelerate(InputAction.CallbackContext context)
     {
-       // if (context.started)
+        // if (context.started)
         //{
-            _accelDir = context.ReadValue<float>();
+        _accelDir = context.ReadValue<float>();
         //}
     }
+
     public void OnTurn(InputAction.CallbackContext context)
     {
-       // if (!context.started) return;
+        // if (!context.started) return;
         var val = context.ReadValue<float>();
         _turnDir = val;
     }
